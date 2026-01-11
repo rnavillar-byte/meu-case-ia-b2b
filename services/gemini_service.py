b04d6f4fbac5cdca17eee569508483a01a8ab790
rnavillar-byte <rnavillar@gmail.com>
2026-01-11 19:03:15 -0300

    gemini_service.py

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -2,10 +2,24 @@
 import requests
 
 def gerar_texto_gemini(prompt, api_key):
-    url = "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent"
-    headers = {"Content-Type": "application/json"}
-    params = {"key": api_key}
-    data = {"contents":[{"parts":[{"text": prompt}]}]}
+    url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    params = {
+        "key": api_key
+    }
+
+    data = {
+        "contents": [
+            {
+                "parts": [
+                    {"text": prompt}
+                ]
+            }
+        ]
+    }
+
     response = requests.post(url, headers=headers, params=params, json=data, timeout=60)
 
     if response.status_code != 200:
